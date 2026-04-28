34c378afb2135dfc044a843a10b94163ce3438be
willheadon <willheadon@yahoo.com>
2026-04-28 13:57:20 +1000

    Create bom.py

---
+++ bom.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "http://www.bom.gov.au/cgi-bin/wrap_fwo.pl?IDN60169.html"
+
+html = requests.get(url).text
+soup = BeautifulSoup(html, "html.parser")
+
+tables = soup.find_all("table")
+table = tables[19]
+
+rows = []
+for tr in table.find_all("tr"):
+    cols = [td.get_text(strip=True) for td in tr.find_all("td")]
+    if cols:
+        rows.append(cols)
+
+df = pd.DataFrame(rows)
+df.to_csv("bom_rainfall.csv", index=False)
